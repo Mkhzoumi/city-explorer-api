0f5320c418ab59b4106367426cf35c06d4ec7e56
Mkhzoumi <zx.hammam@hotmail.com>
2021-06-17 17:36:04 +0300

    cache added

--- controller/Data.controller.js
+++ controller/Data.controller.js
@@ -1,18 +1,28 @@
 const axios = require('axios');
 const Weather = require('../models/Weather.model')
 require('dotenv').config();
-const weatherKey= process.env.WEATHER_API_KEY;
+const weatherKey = process.env.WEATHER_API_KEY;
 const weatherSite = process.env.WEATHER_SITE;
+const Cache = require('../helper/Cache');
+const cacheObj = new Cache();
 
 const data = (req, res) => { // callback function of what we should do with our request
     let lat = req.query.lat;
     let lon = req.query.lon;
+    const reqKey = `weather-${lat}-${lon}`;
+    
     if (lat && lon) {
-        axios.get(`${weatherSite}?key=${weatherKey}&lat=${lat}&lon=${lon}`).then(response => {
-            const responseData = response.data.data.map(obj => new Weather(obj));
-            res.json(responseData)
-        }).catch(error => { res.send(error.message) });
-        // res.json(data) // our endpoint function response
+        if (cacheObj[reqKey] && (Date.now() - cacheObj[reqKey].timestamp < 86400000)) {
+            res.json(cacheObj[reqKey]);
+        } else {
+
+            axios.get(`${weatherSite}?key=${weatherKey}&lat=${lat}&lon=${lon}`).then(response => {
+                const responseData = response.data.data.map(obj => new Weather(obj));
+                cacheObj[reqKey] = responseData;
+                cacheObj[reqKey].timestamp = Date.now();
+                res.json(responseData)
+            }).catch(error => { res.send(error.message) });
+        }
     } else {
         res.send('please provide a valid lat and lon')
     }
